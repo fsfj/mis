0c52611dde22c5e3f7db4574e5411afa499f4d25
JR Feliciano <jr@anynetworksystems.com>
2018-10-01 17:01:24 +0800

    nuxt

--- client/mis-angular/src/app/members/model/member.model.ts
+++
@@ -1,7 +0,0 @@
-export class Member {
-    id: number;
-    firstName: string;
-    middleName: string;
-    lastName: string;
-    dateOfBirth: Date;
-}
---
+++ client/mis-nuxt/pages/members/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>Members Works!</h1>
+</template>
+<script>
+export default {
+    
+}
+</script>
