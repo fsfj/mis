5f56d2526a0c5b8fde5b8fa35d088a1ac39bf6fd
JR Feliciano <jr@anynetworksystems.com>
2018-10-22 17:03:10 +0800

    mis-vue

---
+++ client/mis-vue/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class Home extends Vue {
+  title: string = "Hello World";
+}
+</script>
